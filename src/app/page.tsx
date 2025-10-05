f679823508b7247786d5b6f0f37f5374f732e3d4
Zaeem Nasir 1 <mohammadzaeemnasir1@gmail.com>
2025-10-05 12:54:48 +0000

    on login or refresh, customers tab should open instead of dashboard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import { auth } from '@/lib/auth';
 export default async function RootPage() {
   const session = await auth();
   if (session) {
-    redirect('/dashboard');
+    redirect('/customers');
   } else {
     redirect('/login');
   }
